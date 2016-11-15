f7532a28ccff0586aec52536e538bd668f63ae9a
Roman Blum <rmnblm@gmail.com>
2016-11-15 15:57:14 +0100

    :construction: setting up Xcode project

---
+++ Raclette/Raclette.h
@@ -0,0 +1,19 @@
+//
+//  Raclette.h
+//  Raclette
+//
+//  Created by Roman Blum on 15.11.16.
+//  Copyright © 2016 RMNBLM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Raclette.
+FOUNDATION_EXPORT double RacletteVersionNumber;
+
+//! Project version string for Raclette.
+FOUNDATION_EXPORT const unsigned char RacletteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raclette/PublicHeader.h>
+
+
---
+++ RacletteExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RacletteExample
+//
+//  Created by Roman Blum on 15.11.16.
+//  Copyright © 2016 RMNBLM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
