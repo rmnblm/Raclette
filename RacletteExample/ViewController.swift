a4c2700cf98fb58ad04aa4cf2a5491d8044a3c5f
Roman Blum <rmnblm@gmail.com>
2016-11-16 17:52:42 +0100

    :construction: improve example

--- RacletteExample/ViewController.swift
+++ RacletteExample/ViewController.swift
@@ -19,22 +19,44 @@ class ViewController: UIViewController {
         tableView.tableFooterView = UIView()
         tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
 
+        var firstSection = tableView.addSection()
+        firstSection.headerTitle = "First Section Header"
+        firstSection.footerTitle = "First Section Footer"
+
         let row1 = Row()
         row1.dynamicHeight = true
         row1.configuration = { cell in
             cell.textLabel?.numberOfLines = 0
-            cell.textLabel?.text = "Text Label Text Label Text Label Text Label Text Label Text Label Text Label Text Label Text Label Text Label "
-        }
-
-        tableView.raclette.addRow(row1)
-
-        let row2 = Row<CustomCell>()
-        row2.height = 200
-        row2.configuration = { cell in
-            cell.textLabel?.text = "Text Label"
+            cell.textLabel?.text = "This is a long-ass text which causes the cell to dynamically set the height of the row."
         }
         
-        tableView.raclette.addRow(row2)
+        firstSection.addRow(row1)
+
+        firstSection.createRow { row in
+            row.height = 200
+            row.configuration = { cell in
+                cell.textLabel?.text = "Second Row in First Section with a static height of 200"
+            }
+        }
+
+        var secondSection = tableView.createSection { section in
+            section.headerTitle = "Second Section Header"
+            section.createRow { (row: Row<CustomCell>) in
+                row.configuration = { cell in
+                    cell.textLabel?.text = "Text Label 2"
+                }
+            }
+        }
+
+        secondSection.footerTitle = "Second Section Footer"
+
+        tableView.createRow { row in
+            row.dynamicHeight = true
+            row.configuration = { cell in
+                cell.textLabel?.numberOfLines = 0
+                cell.textLabel?.text = "This row will be automatically added to the the second section"
+            }
+        }
     }
 }
 
