d1af8d673e89f02becf0f38e0e43f85cd0467d12
Roman Blum <rmnblm@gmail.com>
2016-11-17 10:09:59 +0100

    :white_check_mark: add tests for Row and Section

--- RacletteTests/RowTests.swift
+++ RacletteTests/RowTests.swift
@@ -10,5 +10,51 @@ import XCTest
 @testable import Raclette
 
 class RowTests: XCTestCase {
+    func testThat_whenCellIsConfigured_itUsesTheConfigurationClosure() {
+        var configured = false
+        let row = Row()
+        row.configuration = { cell in
+            configured = true
+        }
 
+        XCTAssertFalse(configured)
+
+        row.configure(UITableViewCell())
+        XCTAssertTrue(configured)
+    }
+
+    func thestThat_whenSettingTheReuseIdentifier_itIsTheSameAfterConfiguration() {
+        let staticIdentifier = "MyCell"
+        let row = Row()
+        row.reuseIdentifier = staticIdentifier
+
+        XCTAssertEqual(staticIdentifier, row.reuseIdentifier)
+
+        row.configure(UITableViewCell())
+        XCTAssertEqual(staticIdentifier, row.reuseIdentifier)
+    }
+
+    func thestThat_whenSettingTheReuseIdentifier_itIsTheSameAfterConfigurationOfACustomCell() {
+        let staticIdentifier = "MyCell"
+        let row = Row()
+        row.reuseIdentifier = staticIdentifier
+
+        XCTAssertEqual(staticIdentifier, row.reuseIdentifier)
+
+        row.configure(CustomCell())
+        XCTAssertEqual(staticIdentifier, row.reuseIdentifier)
+    }
+
+    func thestThat_whenNotSettingTheReuseIdentifier_itIsTypeNameOfTheConfiguredCell() {
+        let expectedIdentifier = "CustomCell"
+        let row = Row<CustomCell>()
+        row.configuration = { cell in }
+
+        XCTAssertEqual(expectedIdentifier, row.reuseIdentifier)
+
+        row.configure(CustomCell())
+        XCTAssertEqual(expectedIdentifier, row.reuseIdentifier)
+    }
+
+    class CustomCell: UITableViewCell { }
 }
--- RacletteTests/SectionTests.swift
+++ RacletteTests/SectionTests.swift
@@ -10,5 +10,75 @@ import XCTest
 @testable import Raclette
 
 class SectionTests: XCTestCase {
+    func testThat_whenASectionIsInitialized_itHasZeroRows() {
+        let section = Section()
 
+        XCTAssertEqual(section.rows.count, 0)
+    }
+
+    func testThat_whenAddingARow_itHasOneRow() {
+        let section = Section()
+        section.addRow()
+
+        XCTAssertEqual(section.rows.count, 1)
+    }
+
+    func testThat_whenAddingAPreconfiguredRow_itHasOneRow() {
+        let section = Section()
+        let row = Row()
+        section.addRow(row)
+
+        XCTAssertEqual(section.rows.count, 1)
+    }
+
+    func testThat_whenCreatingARow_itHasOneRow() {
+        let section = Section()
+        section.createRow { row in
+        }
+
+        XCTAssertEqual(section.rows.count, 1)
+    }
+
+    func testThat_whenAddingMultipleRows_itHasMultipleRows() {
+        let times =  10
+        let section = Section()
+        (1...times).forEach { number in
+            section.addRow()
+        }
+
+        XCTAssertEqual(section.rows.count, times)
+    }
+
+    func testThat_whenAddingMultiplePreconfiguredRows_itHasMultipleRows() {
+        let times =  10
+        let section = Section()
+        (1...times).forEach { number in
+            let row = Row()
+            section.addRow(row)
+        }
+
+        XCTAssertEqual(section.rows.count, times)
+    }
+
+    func testThat_whenCreatingMultipleRows_itHasMultipleRows() {
+        let times =  10
+        let section = Section()
+        (1...times).forEach { number in
+            section.createRow { row in
+
+            }
+        }
+
+        XCTAssertEqual(section.rows.count, times)
+    }
+
+    func testThat_whenClearingAllRowsInASection_itHasZeroRows() {
+        let section = Section()
+        section.addRow()
+
+        XCTAssertEqual(section.rows.count, 1)
+
+        section.clearRows()
+        XCTAssertEqual(section.rows.count, 0)
+    }
 }
