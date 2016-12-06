1dfbe548102bed1a76939c94ba03c15aeb5f32ae
Roman Blum <rmnblm@gmail.com>
2016-12-06 13:42:30 +0100

    update example to API changes

--- RacletteExample/ViewController.swift
+++ RacletteExample/ViewController.swift
@@ -16,17 +16,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         tableView.tableFooterView = UIView()
-        tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+        tableView.register(CustomCell.self)
 
         var firstSection = tableView.addSection()
         firstSection.headerTitle = "First Section Header"
         firstSection.footerTitle = "First Section Footer"
 
         let row1 = Row()
-        row1.dynamicHeight = true
         row1.configuration = { cell in
             cell.textLabel?.numberOfLines = 0
-            cell.textLabel?.text = "This is a long-ass text which causes the cell to dynamically set the height of the row."
+            cell.textLabel?.text = "Pinterest cred put a bird on it, meggings blue bottle pop-up la croix pok pok narwhal drinking vinegar hammock man bun VHS tofu freegan. Typewriter marfa tacos, viral lomo narwhal lyft farm-to-table subway tile wolf cold-pressed fam whatever fingerstache. Squid pickled kombucha you probably haven't heard of them neutra. Brooklyn taxidermy cray, helvetica venmo vinyl pop-up green juice pug. Fingerstache aesthetic poke, succulents keytar hammock man braid quinoa narwhal kickstarter drinking vinegar activated charcoal occupy fam stumptown. Ramps tattooed meh fam, freegan gastropub keytar gochujang fanny pack seitan tumblr 3 wolf moon gentrify. Skateboard beard humblebrag woke, swag cornhole kitsch everyday carry sartorial dreamcatcher retro." // Hipster Ipsum
         }
         
         firstSection.addRow(row1)
@@ -35,6 +34,18 @@ class ViewController: UIViewController {
             row.height = 200
             row.configuration = { cell in
                 cell.textLabel?.text = "Second Row in First Section with a static height of 200"
+            }
+        }
+        firstSection.createRow { row in
+            row.configuration = { cell in
+                cell.textLabel?.text = "Overrides the global isRowHighlightingEnabled setting. You can touch me. :)"
+            }
+            row.shouldHighlight = { cell, _ in
+                return true
+            }
+            row.didSelect = { cell, tableInfo in
+                cell.textLabel?.text = "Selected"
+                tableInfo.tableView.deselectRow(at: tableInfo.indexPath, animated: true)
             }
         }
 
@@ -50,12 +61,13 @@ class ViewController: UIViewController {
         secondSection.footerTitle = "Second Section Footer"
 
         tableView.createRow { row in
-            row.dynamicHeight = true
             row.configuration = { cell in
                 cell.textLabel?.numberOfLines = 0
                 cell.textLabel?.text = "This row will be automatically added to the the second section"
             }
         }
+
+        tableView.isRowHighlightingEnabled = false
     }
 }
 
